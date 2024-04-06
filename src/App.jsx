d62af827fbe1487ebbf80ed04882d18445b86607
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-04-06 19:02:18 +0530

    dev commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from "./components/Header/Header.jsx";
 
 function App() {
 
 
   return (
     <>
-
-      <h1 className='bg-green-600 p-4'>React Router</h1>
+        <Header />
     </>
   )
 }
